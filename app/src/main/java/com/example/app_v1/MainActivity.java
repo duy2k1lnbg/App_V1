f8e6af3e87653510111a4fba18b85e64f31838bb
duy2k1lnbg <79616925+duy2k1lnbg@users.noreply.github.com>
2023-03-12 12:31:47 +0700

    add xml

--- app/src/main/java/com/example/app_v1/MainActivity.java
+++ app/src/main/java/com/example/app_v1/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.app_v1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lv_tin_tuc;
+    String link = "https://vtc.vn/rss/thoi-su.rss";
+    String link_2 = "https://vtv.vn/trong-nuoc/xa-hoi.rss";
+    List<Tin_tuc> tin_tucList;
+
+    Button btn_show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_show = findViewById(R.id.btn_show);
+        tin_tucList = new ArrayList<>();
+        lv_tin_tuc = findViewById(R.id.lv_tin_tuc);
+
+
     }
 }
